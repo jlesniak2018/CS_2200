9516973062bd313604733c2e656c6b4cee5f86f0
Joe Lesniak <joseph.f.lesniak@gmail.com>
2016-02-27 10:21:20 -0500

    prj2 updated

---
+++ prj2/Assembly/test.s
@@ -0,0 +1,23 @@
+addi $a0, $zero, 7
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+la $s0, jump
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+jalr $s0, $ra
+
+
+
+jump: add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $a1, $a0, $a0
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
