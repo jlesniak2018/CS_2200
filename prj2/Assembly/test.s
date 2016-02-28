2350f0df8cc0a8643ecf54e0e1370e8b9fe4f173
Joe Lesniak <joseph.f.lesniak@gmail.com>
2016-02-27 21:39:28 -0500

    prj2 updated

--- prj2/Assembly/test.s
+++ prj2/Assembly/test.s
@@ -1,23 +1,10 @@
-addi $a0, $zero, 7
+beq $zero, $zero, loop
+addi $a4, $zero, 9
+sw $a4, 0x4($zero)
 add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-la $s0, jump
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-jalr $s0, $ra
-
-
-
-jump: add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $a1, $a0, $a0
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
-add $zero, $zero, $zero
+loop: addi $a1, $zero, 9
+add $a2, $a1, $a1
+sw $a2, 0x0($zero)
+lw $a3, 0x0($zero)
+addi $a3, $a3, 2
+sw $a3, 0x0($zero)
